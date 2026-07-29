96190bd07f622dfdae0a5da6bf09bacfa52e02ce
cuentasena <josedavidlemusrestrepo5@gmail.com>
2026-07-29 15:56:23 -0500

    Dp estructura basica del proyecto

---
+++ ApiMesaAyuda/src/test/java/com/david/ApiMesaAyuda/ApiMesaAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.ApiMesaAyuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMesaAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

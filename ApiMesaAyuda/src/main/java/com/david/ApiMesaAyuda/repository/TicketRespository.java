2991bcafb4a4765887ce76c7d042301fecea1815
durandavid159-arte <durandavid159@gmail.com>
2026-08-02 19:07:04 -0500

    DP creo la carpeta respository y creo las clases que necesito

---
+++ ApiMesaAyuda/src/main/java/com/david/ApiMesaAyuda/repository/TicketRespository.java
@@ -0,0 +1,16 @@
+package com.david.ApiMesaAyuda.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.david.ApiMesaAyuda.entity.EstadoTicket;
+import com.david.ApiMesaAyuda.entity.Ticket;
+import com.david.ApiMesaAyuda.entity.Usuario;
+
+public interface TicketRespository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findByCreadorPorOrderByCreadoEnDesc (Usuario creadorPor);
+    List<Ticket> findByEstadoNotAndSlaVenceEnBefore(EstadoTicket estado, LocalDateTime fecha);
+}
---
+++ ApiMesaAyuda/src/main/java/com/david/ApiMesaAyuda/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.david.ApiMesaAyuda.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.david.ApiMesaAyuda.entity.Usuario;
+
+
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
+    Optional <Usuario> findfindByEmail(String email);
+
+    boolean existexistsByEmail(String email);
+}
